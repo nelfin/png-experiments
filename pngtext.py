c0d3ceebcf8a0278eacce5f35dc17df9cebc4ab4
Andrew Haigh <hello@nelf.in>
2025-11-23 12:02:50 +1300

    Add pngtext

---
+++ pngtext.py
@@ -0,0 +1,71 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#     "pypng",
+# ]
+# ///
+"""
+pngtext
+~~~~~~~
+
+Insert or extract a custom file into a PNG .iTXt chunk
+
+Usage:
+
+    pngtext graph.png < graphviz.dot
+    pngtext -x application/graphviz graph.png | dot -Tsvg
+
+"""
+
+import argparse
+import struct
+import zlib
+from functools import partial
+
+import png
+
+def info(tag, chunk):
+    if tag in (b"tEXt", b"iTXt", b"zTXt"):
+        mimetype = chunk[:chunk.find(b"\x00")].decode("latin-1")
+        return mimetype
+
+
+def process(tag, chunk, target_mimetype="text/plain"):
+    mimetype, idx = "", chunk.find(b"\x00")
+    if tag == b"tEXt" or tag == b"iTXt":
+        mimetype, data = chunk[:idx], chunk[idx+1:]
+        mimetype = mimetype.decode("latin-1")
+    if tag == b"zTXt":
+        mimetype, method, zdata = chunk[:idx], chunk[idx+1], chunk[idx+2:]
+        assert method == 0, f"Method {method} unknown (0=DEFLATE)"
+        mimetype = mimetype.decode("latin-1")
+        func = zlib.decompress
+    if mimetype == target_mimetype:
+        if tag == b"tEXt":
+            return data.decode("latin-1")
+        elif tag == b"iTXt":
+            return data.decode("utf-8")
+        elif tag == b"zTXt":
+            return func(zdata).decode("latin-1")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image', type=argparse.FileType('rb'), help="PNG image")
+parser.add_argument('text', type=argparse.FileType('rb'), nargs='?', default="-")
+parser.add_argument('-x', '--extract', metavar="MIME", default='text/plain', help="Extract this mimetype")
+parser.add_argument("-l", "--list", action="store_true", help="List all mimetypes")
+
+args = parser.parse_args()
+
+if args.list:
+    func = info
+else:
+    func = partial(process, target_mimetype=args.extract)
+
+# actually this is probably a waste of time, ref pngtools e.g. pngchunks(1) instead
+# pngcrush can insert zTXt no problem, easy Python utils? What about PIL?
+
+im = png.Reader(args.image)
+for tag, data in im.chunks():
+    if (result := func(tag, data)):
+        print(result)
